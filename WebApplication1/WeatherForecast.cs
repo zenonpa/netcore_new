c254f9df080e2be799796fefd0783c7402f36845
Zenon Paredes <zenonpa@hotmail.com>
2023-02-06 22:22:31 -0500

    Comentarios 1

--- WebApplication1/WeatherForecast.cs
+++ WebApplication1/WeatherForecast.cs
@@ -5,8 +5,10 @@ namespace WebApplication1
         //TEST
         public DateOnly Date { get; set; }
 
+        //Temperatura en grados Celsius
         public int TemperatureC { get; set; }
 
+        //Temperatura en grados Fahrenheit
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
